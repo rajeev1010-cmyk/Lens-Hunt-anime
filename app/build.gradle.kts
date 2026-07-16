00aea7e78f716b235e5d75d0fce2e8aa7d307f05
rajeev1010-cmyk <rajeev1010@gmail.com>
2026-07-17 00:32:43 +0530

    chore: refactor package name and update versioning
    Update namespace and application ID to com.gianthunt.lenshunt, increment version to 1.0.1, and clean up obsolete test files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,15 +10,15 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.gianthunt.lenshunt"
   compileSdk = 35
 
   defaultConfig {
-    applicationId = "com.aistudio.lenshunt.rjvhnt"
+    applicationId = "com.gianthunt.lenshunt"
     minSdk = 24
     targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
